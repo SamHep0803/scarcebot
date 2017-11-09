23f573e372c8c789775bd9cd63f424e7018cb2f4
SamHep0803 <samhep0803@gmail.com>
2017-11-09 16:56:00 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js');
-const Client = new Discord.Client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log('Ready to make videos!');
   client.user.setGame("making videos.");
+});
+
+client.on('ready', () => {
+  if (message.content === 's.makeavideo') {
+    message.channel.send('Hey whatsup guys, scarce here.');
+  };
 });
 
 client.login('Mzc4MTYwMjU2NTM2MDE4OTQ0.DOXcvA.SR2K66NI9pKNknG9xuDjzkTM5yI');
